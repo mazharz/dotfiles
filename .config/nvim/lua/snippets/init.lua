a70316cda818ebaca0eb0199647e7a77e361ea69
Mazhar Zandsalimi <m47h4r@gmail.com>
2025-10-20 08:55:23 +0330

    fix(snippets): keybinds to work w/ tmux

--- .config/nvim/lua/snippets/init.lua
+++ .config/nvim/lua/snippets/init.lua
@@ -3,25 +3,25 @@ local ls = require("luasnip")
 -- to jump between snippet sections
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set({ "i", "s" }, "<C-S-j>", function()
+vim.keymap.set({ "i", "s" }, "<C-A-j>", function()
 	if ls.expand_or_jumpable() then
 		ls.expand_or_jump()
 	end
 end, opts)
 
-vim.keymap.set({ "i", "s" }, "<C-S-k>", function()
+vim.keymap.set({ "i", "s" }, "<C-A-k>", function()
 	if ls.jumpable(-1) then
 		ls.jump(-1)
 	end
 end, opts)
 
-vim.keymap.set({ "i", "s" }, "<C-S-l>", function()
+vim.keymap.set({ "i", "s" }, "<C-A-l>", function()
 	if ls.choice_active() then
 		ls.change_choice(1)
 	end
 end, opts)
 
-vim.keymap.set({ "i", "s" }, "<C-S-h>", function()
+vim.keymap.set({ "i", "s" }, "<C-A-h>", function()
 	if ls.choice_active() then
 		ls.change_choice(-1)
 	end
