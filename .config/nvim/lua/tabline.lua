34e6c62f39f1315aed25b5db57184965c5c323e6
Mazhar Zandsalimi <m47h4r@gmail.com>
2025-11-30 17:13:22 +0330

    feat(nvim): improve tabline

--- .config/nvim/lua/tabline.lua
+++ .config/nvim/lua/tabline.lua
@@ -1,15 +1,19 @@
---- Generate tab item
---- @param index number
---- @param max_width number
+local PADDING = 1
+
+--- @class Tab
+--- @field name string
+--- @field length number
+--- @field is_modified boolean
+--- @field filetype string
+
+--- Format tab item
+--- @param tab Tab
 --- @return string
-local function tab_item(index, max_width)
-	max_width = max_width - 2 -- for the padding spaces
-	local buf_list = vim.fn.tabpagebuflist(index)
-	local win = vim.fn.tabpagewinnr(index)
-	local bufnr = buf_list[win]
-	local name = vim.fn.bufname(bufnr)
-	local is_modified = vim.api.nvim_get_option_value("modified", { buf = bufnr })
-	local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+local function format_tab(tab)
+	local name = tab.name
+	local length = tab.length - (PADDING * 2)
+	local is_modified = tab.is_modified
+	local filetype = tab.filetype
 
 	if name == "" then
 		name = "[No Name]"
@@ -25,11 +29,78 @@ local function tab_item(index, max_width)
 		name = name .. " ●"
 	end
 
-	if #name > max_width then
-		name = name:sub(#name - max_width, #name)
+	if #name > length then
+		name = name:sub(#name - length, #name)
 	end
 
 	return " " .. name .. " "
+end
+
+--- Get current tab
+--- @param index number
+--- @return Tab
+local function current_tab_buffer_info(index)
+	local buf_list = vim.fn.tabpagebuflist(index)
+	local win = vim.fn.tabpagewinnr(index)
+	local bufnr = buf_list[win]
+	local name = vim.fn.bufname(bufnr)
+
+	local length = #vim.fn.fnamemodify(name, ":t") + (PADDING * 2)
+
+	local is_modified = vim.api.nvim_get_option_value("modified", { buf = bufnr })
+
+	local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+
+	return {
+		name = name,
+		length = length,
+		is_modified = is_modified,
+		filetype = filetype,
+	}
+end
+
+--- Get the surplus info
+--- @return number[], number
+local function calculate_surplus()
+	local screen_width = vim.o.columns
+	local number_of_tabs = vim.fn.tabpagenr("$")
+
+	local lengths = {}
+	local total_length = 0
+	for index = 1, number_of_tabs do
+		local tab = current_tab_buffer_info(index)
+		total_length = total_length + tab.length
+		table.insert(lengths, tab.length)
+	end
+
+	local surplus = total_length - screen_width
+
+	local fractions = vim.tbl_map(function(length)
+		return length / total_length
+	end, lengths)
+
+	return fractions, surplus
+end
+
+--- Apply length reduction to tab
+--- @param tab Tab
+--- @param surplus_fraction number
+--- @param surplus number
+--- @return Tab
+local function apply_length_reduction(tab, surplus_fraction, surplus)
+	if surplus <= 0 then
+		return tab
+	end
+
+	local surplus_by_fraction = math.ceil(surplus * surplus_fraction)
+	local length = tab.length - surplus_by_fraction
+
+	return {
+		name = tab.name,
+		length = length,
+		is_modified = tab.is_modified,
+		filetype = tab.filetype,
+	}
 end
 
 --- Generate tabline
@@ -37,11 +108,12 @@ end
 function _G.generate_tabline()
 	local result = ""
 
-	local screen_width = vim.o.columns
 	local number_of_tabs = vim.fn.tabpagenr("$")
-	local max_tab_width = math.floor(screen_width / number_of_tabs)
+	local surplus_fractions, surplus = calculate_surplus()
 
 	for index = 1, number_of_tabs do
+		local tab = apply_length_reduction(current_tab_buffer_info(index), surplus_fractions[index], surplus)
+
 		-- set the highlight
 		if index == vim.fn.tabpagenr() then
 			result = result .. "%#TabLineSel#"
@@ -52,7 +124,7 @@ function _G.generate_tabline()
 		-- setting tab number (to be able to use with mouse)
 		result = result .. "%" .. index .. "T"
 
-		result = result .. tab_item(index, max_tab_width)
+		result = result .. format_tab(tab)
 	end
 
 	-- reset highlight and mark end of last tab number (for mouse)
