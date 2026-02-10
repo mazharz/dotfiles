ea58c715cac746bf4262315714737b6be65dbae7
Mazhar Zandsalimi <m47h4r@gmail.com>
2026-02-10 08:38:16 +0300

    update

--- .config/waybar/modules/custom/volume.sh
+++ .config/waybar/modules/custom/volume.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -f 2 -d '.')
+volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -f 2 -d ' ' | sed 's/\.\|^0//g')
 
 echo -n " ${volume}%"
