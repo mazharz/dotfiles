808d42c078a2e253db5de1624f8c8ee90c393168
Mazhar Zandsalimi <m47h4r@gmail.com>
2026-05-17 09:33:09 +0330

    feat(nvim): configure zen-mode

--- home/.config/nvim/lua/plugins/zen-mode.lua
+++ home/.config/nvim/lua/plugins/zen-mode.lua
@@ -3,4 +3,19 @@ return {
 	keys = {
 		{ "<leader>zz", ":ZenMode<CR>" },
 	},
+	opts = {
+		window = {
+			backdrop = 1, -- don't shade backdrop
+		},
+		plugins = {
+			options = { laststatus = 0 },
+			tmux = { enabled = true },
+		},
+		on_open = function()
+			os.execute("hyprctl dispatch 'hl.dsp.window.fullscreen()'")
+		end,
+		on_close = function()
+			os.execute("hyprctl dispatch 'hl.dsp.window.fullscreen()'")
+		end,
+	},
 }
